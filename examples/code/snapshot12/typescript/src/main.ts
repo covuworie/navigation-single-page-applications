06bd6bd170256a5303681f1dec7e88d69bec71e9
Chuk Ovuworie <chuk.ovuworie@dotevoke.com>
2020-05-04 12:37:43 -0500

    Add code for snapshot 12

---
+++ examples/code/snapshot12/typescript/src/main.ts
@@ -0,0 +1,14 @@
+// Updates dynamic content based on the fragment identifier.
+function navigate() {
+  // Get a reference to the "content" div.
+  const contentDiv = document.querySelector("#content") as HTMLDivElement;
+
+  // Set the "content" div to contain the current hash value.
+  contentDiv.textContent = location.hash;
+}
+
+// Navigate once to the initial hash value.
+navigate();
+
+// Navigate whenever the fragment identifier value changes.
+window.addEventListener("hashchange", navigate);
